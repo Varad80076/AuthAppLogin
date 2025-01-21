86113d0b2dfa6aaa7525bc220b969aef0dea0bd6
Varad80076 <badgujarvarad800@gmail.com>
2025-01-21 10:59:17 +0530

    origin2

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,6 @@ app.use(
       const allowedOrigins = [
         process.env.FRONTEND_URL,
         process.env.FRONTEND_URL2,
-        "https://auth-app-sepia-pi.vercel.app",
         "https://auth-app-sepia-pi.vercel.app/",
         "http://localhost:5173"
       ];
