9dcda26c23ee088844a3d5e32a7643a62f528c0f
Varad80076 <badgujarvarad800@gmail.com>
2025-01-21 10:00:46 +0530

    vercel

--- Backend/index.js
+++ Backend/index.js
@@ -9,27 +9,33 @@ const authRouter = require("./Routes/AuthRouter");
 require("dotenv").config();
 //database connectivity using calling mongodb veriable
 mongodb;
+// app.use(cors());
 //apply cors validation for client and server verification
-console.log(process.env.FRONTEND_URL)
-
-const corsOptions ={
+app.use(
+  cors({
     origin: [
-      process.env.FRONTEND_URL,
-      process.env.FRONTEND_URL2,
-      "http://localhost:5173"
+      process.env.FRONTEND_URL
     ], // Frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
-    allowedHeaders: 'Content-Type,Authorization', // Allowed headers
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
     credentials: true, // Allow cookies if needed
-  }
+    // mode: "no-cors",
+  })
+);
 
-app.use(cors(corsOptions));
 
-// This line of code enables your Express application to parse incoming JSON data in request bodies.
+
+// This line of code enables your Express application to parse incoming JSON data in request bodies.it is Generally use in middleware
 app.use(bodyParser.json());
 app.use(express.json());
 
 //Routing using /auth
+
+app.use("/", console.log('server is running'));
+app.use("/auth", authRouter);
+
+//server in listening in port no
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
 
 app.get("/varad", async (req, res) => {
   res.send('server is running')
