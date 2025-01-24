feb7a39c2ff950523c3ff0a0b208747aa8844a53
Varad80076 <badgujarvarad800@gmail.com>
2025-01-24 19:19:49 +0530

    frontend api updated

--- Frontend/Login/src/util/allAPIs.js
+++ Frontend/Login/src/util/allAPIs.js
@@ -1,4 +1,4 @@
-const host = import.meta.env.VITE_API_KEY2;
+const host = import.meta.env.VITE_API_KEY;
 export const signup = `${host}auth/signup`;
 export const login = `${host}auth/login`;
 export const varad = `${host}auth/varad`;
