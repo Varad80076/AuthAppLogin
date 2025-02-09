01eaff7ebe84fc977bcb947bbf7add2ef0221abe
Varad80076 <badgujarvarad800@gmail.com>
2025-02-09 21:32:20 +0530

    updated code

--- Frontend/Login/src/Component/AllRoutes.jsx
+++ Frontend/Login/src/Component/AllRoutes.jsx
@@ -3,15 +3,14 @@ import NextComponent from '../Component/NextComponent';
 import SignUp from '../Component/SignUp';
 import Login from '../Component/Login';
 import Forget from '../Component/Forget';
-import Password from './password';
+import Password from '../Component/Password';
 
 function AllRoutes() {
   return (
     <div className='flex flex-col items-center justify-center h-fit gap-10 w-full min-h-screen bg-gradient-to-br from-blue-200 to-blue-400 '> 
       <Routes>
-        <Route path="/signup" element={<SignUp />} />
         <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/next" element={<NextComponent />} />
         <Route path="/forget" element={<Forget />} />
         <Route path="/password" element={<Password />} />
